61614cfd659e00d970f71b174e8779ff5287578f
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-03-10 09:01:25 -0300

    Adição de mais uma tela.

--- app/src/main/java/br/senai/sp/jandira/imc/IMCActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/imc/IMCActivity.kt
@@ -2,19 +2,16 @@
 package br.senai.sp.jandira.imc
 
 import android.content.Intent
-import android.content.IntentSender
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class IMCActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_imc)
 
         // Instanciando as classes referentes aos componentes(View) da activity.
         val editTextPeso = findViewById<EditText>(R.id.edit_text_peso)
@@ -23,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         // Cria um ouvinte para o botão abrir, que vai abriroutra activity.
         buttonCalcular.setOnClickListener {
-            val abrirActivityResultado = Intent(this, ResultadoActivity::class.java)
+            val abrirActivityResultado = Intent(this, ResultadoIMCActivity::class.java)
 
             abrirActivityResultado.putExtra("peso", editTextPeso.text.toString())
             abrirActivityResultado.putExtra("altura", editTextAltura.text.toString())
--- app/src/main/java/br/senai/sp/jandira/imc/ResultadoIMCActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/imc/ResultadoIMCActivity.kt
@@ -1,16 +1,14 @@
 package br.senai.sp.jandira.imc
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Toast
 
-class ResultadoActivity : AppCompatActivity() {
+class ResultadoIMCActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_resultado)
+        setContentView(R.layout.activity_imc_resultado)
 
         var peso = intent.getStringExtra("peso")
         var altura = intent.getStringExtra("altura")
