671439a1e1e0ba7525a92496788c22e112f12031
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-03-13 16:56:40 -0300

    Adição de mais uma tela e continuidade das anteriores.

--- app/src/main/java/br/senai/sp/jandira/imc/CalculadoraSaudavelActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/imc/CalculadoraSaudavelActivity.kt
@@ -1,11 +1,28 @@
 package br.senai.sp.jandira.imc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class CalculadoraSaudavelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saudavel_calculadora)
+
+        val buttonNecessidadesCaloricas = findViewById<Button>(R.id.button_necessidades_caloricas)
+        val buttonIMC = findViewById<Button>(R.id.button_imc)
+
+        buttonIMC.setOnClickListener {
+            val abrirActivityIMC = Intent(this, IMCActivity::class.java)
+
+            startActivity(abrirActivityIMC)
+        }
+
+        buttonNecessidadesCaloricas.setOnClickListener {
+            val abrirActivityNecessidadesCalorica = Intent(this, NecessidadeCaloricaActivity::class.java)
+
+            startActivity(abrirActivityNecessidadesCalorica)
+        }
     }
 }
