f195bfa112fbd9bf36cb28d4de904d5ba7c1c214
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-03-19 13:05:08 -0300

    Finalização da estética das telas (até o presente momento, afinal sempre há algo mais para ser adicionado).

--- app/src/main/java/br/senai/sp/jandira/imc/NecessidadeCaloricaActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/imc/NecessidadeCaloricaActivity.kt
@@ -1,6 +1,8 @@
 package br.senai.sp.jandira.imc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class NecessidadeCaloricaActivity : AppCompatActivity() {
@@ -8,5 +10,13 @@ class NecessidadeCaloricaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_caloricas_necessidades)
+
+        val buttonCalcular = findViewById<Button>(R.id.button_calcular)
+
+        buttonCalcular.setOnClickListener{
+            val abrirResultadoNecessidadeCaloricaActivity = Intent(this, ResultadoNecessidadeCaloricaActivity::class.java)
+
+            startActivity(abrirResultadoNecessidadeCaloricaActivity)
+        }
     }
 }
---
+++ app/src/main/java/br/senai/sp/jandira/imc/ResultadoNecessidadeCaloricaActivity.kt
@@ -0,0 +1,12 @@
+package br.senai.sp.jandira.imc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadoNecessidadeCaloricaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado_necessidade_calorica)
+    }
+}
